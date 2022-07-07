7226f54c015de67921f567a4618be8fbfa892dfa
Sergey Olshansky <s.y.olshansky@gmail.com>
2022-07-07 12:40:35 +0300

    incr-12

--- internal/server.go
+++ internal/server.go
@@ -223,6 +223,10 @@ func (s *Server) updatesJSONMetricHandler(w http.ResponseWriter, r *http.Request
 		s.metrics.SetMetric(metricPayload.Name, payload)
 	}
 	w.Header().Add("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(metricPayloads)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 }
 
 func (s *Server) valueJSONMetricHandler(w http.ResponseWriter, r *http.Request) {
